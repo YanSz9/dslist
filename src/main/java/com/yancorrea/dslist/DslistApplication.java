0077c29ab8cb1d59e21081dc5ec9513c00ccb91d
Yan Souza <yansouza009@gmail.com>
2023-05-22 09:02:40 -0300

    feat: swagger

--- src/main/java/com/yancorrea/dslist/DslistApplication.java
+++ src/main/java/com/yancorrea/dslist/DslistApplication.java
@@ -1,7 +1,11 @@
 package com.yancorrea.dslist;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DslistApplication {
@@ -9,5 +13,15 @@ public class DslistApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DslistApplication.class, args);
 	}
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("Game List API")
+						.version("0.11")
+						.description("Sample app Spring Boot 3 with Swagger")
+						.termsOfService("http://swagger.io/terms/")
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 
+	}
 }
